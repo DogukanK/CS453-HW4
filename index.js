b308722a8ea77679b7c23a8c772bcbbcdeb92be4
Doğukan Kurnaz <dogukanertunga@gmail.com>
2022-05-01 14:19:34 +0300

    pass control changed

--- index.js
+++ index.js
@@ -19,12 +19,14 @@ function login() {
             else {
                 //if invalid then put message in the div with id 'error-message'
                 $("#password").focus();
+                /*
                 var obj = $("#error-message").text("Password must be:" + "\n" 
                 + "1. At least 8 characters" + "\n" 
                 + "2. At least one uppercase letter" + "\n" 
                 + "3. At least one lowercase letter" + "\n" 
                 + "4. At least one number" + "\n" 
-                + "5. At least one special character (@$+!%*?&)");           
+                + "5. At least one special character (@+)");  
+                */         
             }
             //replace the \n with <br>.
             obj.html(obj.html().replace(/\n/g, '<br />'));
@@ -42,7 +44,27 @@ function isValidEmailAddress(emailAddress) {
 }
 //check password is valid or not
 function isValidPassword(password) {
-    var pattern = new RegExp(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$+!%*?&])[A-Za-z\d@$+!%*?&]{8,}$/);
+    var pattern = new RegExp(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[A-Za-z\d@+]{8,}$/);
+    if ($.trim(password).length <= 8) {
+        $("#password").focus();
+        $("#error-message").text("At least 8 characters.");
+    }
+    else if ( !password.match(/\d/) ){
+        $("#password").focus();
+        $("#error-message").text("At least one number.");
+    }
+    else if ( !password.match(/[A-Z]/) ){
+        $("#password").focus();
+        $("#error-message").text("At least one uppercase letter.");
+    }
+    else if ( !password.match(/[a-z]/) ){
+        $("#password").focus();
+        $("#error-message").text("At least one lowercase letter.");
+    }
+    else if( !password.match(/^[a-zA-Z0-9+@]+$/)){
+        $("#password").focus();
+        $("#error-message").text("Password can be consists only numbers, letters, and ‘+’, ‘@’");
+    }
     return pattern.test(password);
 }
 
