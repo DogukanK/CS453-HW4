4568a92f48e04c97b23058b1d5a477cb591770e6
Doğukan Kurnaz <dogukanertunga@gmail.com>
2022-05-01 22:32:07 +0300

    last revv

--- Part_2/test.js
+++ Part_2/test.js
@@ -6,11 +6,12 @@ const webdriver = require('selenium-webdriver');
 //Automate the initializing of the standalone wiremock server. It may cause some problems in other OS's other than MacOS with M1 chip.
 //If you face any problem, please comment the process execution block, then start the wiremock from cli manually.
 var process = require('child_process');
-process.exec('java -jar Part_2/wiremock-jre8-standalone-2.33.2.jar',function (err,stdout,stderr) {
+let pc = process.exec('java -jar Part_2/wiremock-jre8-standalone-2.33.2.jar',function (err,stdout,stderr) {
     if(err){
         console.log(err);
     }
-    console.log(stdout); 
+    console.log(stdout);
+    
 });
 
 
@@ -27,7 +28,9 @@ let driver = new webdriver.Builder()
 
 
 async function test() {
-    //go to the url
+    //resize window
+    await driver.manage().window().setRect({width: 700, height: 840})
+
     driver.get("http://localhost:5500/Part_2/index.html");
     //wait until the page is loaded
     await driver.sleep(1000);
@@ -37,17 +40,17 @@ async function test() {
 
     //select email field
     driver.findElement(By.id("email")).sendKeys("admin@admin.com");
-    await driver.sleep(2000);
+    await driver.sleep(1000);
     //select password field
     driver.findElement(By.id("password")).sendKeys("@Dmin1234");
-    await driver.sleep(2000);
+    await driver.sleep(1000);
     //click login button
     driver.findElement(By.id("login")).click();
-    await driver.sleep(2000);
+    await driver.sleep(1000);
 
     //compare the result
-    driver.wait(until.elementLocated(By.id("error-message"), 3000));
-    await driver.sleep(2000);
+    driver.wait(until.elementLocated(By.id("error-message"), 1000));
+    await driver.sleep(1000);
     driver.findElement(By.id("error-message")).getText().then(function (text) {
         if (text == "Login Success!") {
             console.log("Login Success!");
@@ -60,22 +63,22 @@ async function test() {
     /* INVALID EMAIL */
 
     //refresh the page
-    await driver.sleep(2000);
+    await driver.sleep(1000);
     driver.navigate().refresh();
-    await driver.sleep(2000);
+    await driver.sleep(1000);
     //input invalid email
     driver.findElement(By.id("email")).sendKeys("adad323asda");
-    await driver.sleep(2000);
+    await driver.sleep(1000);
     //input valid password
     driver.findElement(By.id("password")).sendKeys("@Dmin1234");
-    await driver.sleep(2000);
+    await driver.sleep(1000);
     //click login button
     driver.findElement(By.id("login")).click();
-    await driver.sleep(2000);
+    await driver.sleep(1000);
 
 
     //compare the results
-    driver.wait(until.elementLocated(By.id("error-message"), 3000));
+    driver.wait(until.elementLocated(By.id("error-message"), 1000));
     driver.findElement(By.id("error-message")).getText().then(function (text) {
         //console.log(text);
         if (text == "Email is not valid") {
@@ -88,22 +91,22 @@ async function test() {
      /* INVALID PASS 1 */
 
     //refresh the page
-    await driver.sleep(2000);
+    await driver.sleep(1000);
     driver.navigate().refresh();
-    await driver.sleep(2000);
+    await driver.sleep(1000);
     //input invalid email
     driver.findElement(By.id("email")).sendKeys("admin@admin.com");
-    await driver.sleep(2000);
+    await driver.sleep(1000);
     //input valid password
     driver.findElement(By.id("password")).sendKeys("1234aaaa");
-    await driver.sleep(2000);
+    await driver.sleep(1000);
     //click login button
     driver.findElement(By.id("login")).click();
-    await driver.sleep(2000);
+    await driver.sleep(1000);
 
 
     //compare the results
-    driver.wait(until.elementLocated(By.id("error-message"), 3000));
+    driver.wait(until.elementLocated(By.id("error-message"), 1000));
     driver.findElement(By.id("error-message")).getText().then(function (text) {
         //console.log(text);
         if (text == "At least one uppercase letter.") {
@@ -116,22 +119,22 @@ async function test() {
      /* INVALID PASS 2 */
 
     //refresh the page
-    await driver.sleep(2000);
+    await driver.sleep(1000);
     driver.navigate().refresh();
-    await driver.sleep(2000);
+    await driver.sleep(1000);
     //input invalid email
     driver.findElement(By.id("email")).sendKeys("admin@admin.com");
-    await driver.sleep(2000);
+    await driver.sleep(1000);
     //input valid password
     driver.findElement(By.id("password")).sendKeys("AAAA1234");
-    await driver.sleep(2000);
+    await driver.sleep(1000);
     //click login button
     driver.findElement(By.id("login")).click();
-    await driver.sleep(2000);
+    await driver.sleep(1000);
 
 
     //compare the results
-    driver.wait(until.elementLocated(By.id("error-message"), 3000));
+    driver.wait(until.elementLocated(By.id("error-message"), 1000));
     driver.findElement(By.id("error-message")).getText().then(function (text) {
         //console.log(text);
         if (text == "At least one lowercase letter.") {
@@ -144,22 +147,22 @@ async function test() {
      /* INVALID PASS 3 */
 
     //refresh the page
-    await driver.sleep(2000);
+    await driver.sleep(1000);
     driver.navigate().refresh();
-    await driver.sleep(2000);
+    await driver.sleep(1000);
     //input invalid email
     driver.findElement(By.id("email")).sendKeys("admin@admin.com");
-    await driver.sleep(2000);
+    await driver.sleep(1000);
     //input valid password
     driver.findElement(By.id("password")).sendKeys("a1s2");
-    await driver.sleep(2000);
+    await driver.sleep(1000);
     //click login button
     driver.findElement(By.id("login")).click();
-    await driver.sleep(2000);
+    await driver.sleep(1000);
 
 
     //compare the results
-    driver.wait(until.elementLocated(By.id("error-message"), 3000));
+    driver.wait(until.elementLocated(By.id("error-message"), 1000));
     driver.findElement(By.id("error-message")).getText().then(function (text) {
         //console.log(text);
         if (text == "At least 8 characters.") {
@@ -172,22 +175,22 @@ async function test() {
     /* INVALID PASS 4 */
 
     //refresh the page
-    await driver.sleep(2000);
+    await driver.sleep(1000);
     driver.navigate().refresh();
-    await driver.sleep(2000);
+    await driver.sleep(1000);
     //input invalid email
     driver.findElement(By.id("email")).sendKeys("admin@admin.com");
-    await driver.sleep(2000);
+    await driver.sleep(1000);
     //input valid password
     driver.findElement(By.id("password")).sendKeys("DoooDooo");
-    await driver.sleep(2000);
+    await driver.sleep(1000);
     //click login button
     driver.findElement(By.id("login")).click();
-    await driver.sleep(2000);
+    await driver.sleep(1000);
 
 
     //compare the results
-    driver.wait(until.elementLocated(By.id("error-message"), 3000));
+    driver.wait(until.elementLocated(By.id("error-message"), 1000));
     driver.findElement(By.id("error-message")).getText().then(function (text) {
         //console.log(text);
         if (text == "At least one number.") {
@@ -200,22 +203,22 @@ async function test() {
     /* INVALID PASS 5 */
 
     //refresh the page
-    await driver.sleep(2000);
+    await driver.sleep(1000);
     driver.navigate().refresh();
-    await driver.sleep(2000);
+    await driver.sleep(1000);
     //input invalid email
     driver.findElement(By.id("email")).sendKeys("admin@admin.com");
-    await driver.sleep(2000);
+    await driver.sleep(1000);
     //input valid password
     driver.findElement(By.id("password")).sendKeys("Do@oDoo^o1");
-    await driver.sleep(2000);
+    await driver.sleep(1000);
     //click login button
     driver.findElement(By.id("login")).click();
-    await driver.sleep(2000);
+    await driver.sleep(1000);
 
 
     //compare the results
-    driver.wait(until.elementLocated(By.id("error-message"), 3000));
+    driver.wait(until.elementLocated(By.id("error-message"), 1000));
     driver.findElement(By.id("error-message")).getText().then(function (text) {
         //console.log(text);
         if (text == "Password can be consists only numbers, letters, and ‘+’, ‘@’") {
@@ -228,8 +231,8 @@ async function test() {
 
     //close the browser after the tests
     await driver.sleep(2000);
-    driver.quit();
-    
+    //close wiremock server
+    pc.kill();
 
 }
 
